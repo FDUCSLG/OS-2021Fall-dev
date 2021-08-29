fc3e2e3019ca81f35359d03bb53d3b8fd8927ac1
riteme <riteme@qq.com>
2021-08-29 22:33:28 +0800

    common: add `memset`, `strncpy`, `strlen`.

--- src/common/string.c
+++ src/common/string.c
@@ -1,25 +1,23 @@
 #include <common/string.h>
 
-void *memcpy(void *restrict dest, const void *restrict src, size_t n) {
-    uint8_t *dest_ptr = (uint8_t *)dest;
-    uint8_t *src_ptr = (uint8_t *)src;
+void *memset(void *s, int c, size_t n) {
+    for (size_t i = 0; i < n; i++)
+        ((uint8_t *)s)[i] = c & 0xff;
 
-    while (n > 0) {
-        *dest_ptr++ = *src_ptr++;
-        n--;
-    }
+    return s;
+}
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n) {
+    for (size_t i = 0; i < n; i++)
+        ((uint8_t *)dest)[i] = ((uint8_t *)src)[i];
 
     return dest;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-    uint8_t *s1_ptr = (uint8_t *)s1;
-    uint8_t *s2_ptr = (uint8_t *)s2;
-
-    while (n > 0) {
-        int c1 = *s1_ptr++;
-        int c2 = *s2_ptr++;
-        n--;
+    for (size_t i = 0; i < n; i++) {
+        int c1 = ((uint8_t *)s1)[i];
+        int c2 = ((uint8_t *)s2)[i];
 
         if (c1 != c2)
             return c1 - c2;
@@ -28,17 +26,41 @@ int memcmp(const void *s1, const void *s2, size_t n) {
     return 0;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-    while (n > 0) {
-        int c1 = *s1++;
-        int c2 = *s2++;
-        n--;
+char *strncpy(char *restrict dest, const char *restrict src, size_t n) {
+    size_t i = 0;
+    for (; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+    for (; i < n; i++)
+        dest[i] = '\0';
 
-        if (c1 != c2)
-            return c1 - c2;
-        if (c1 == 0 || c2 == 0)
+    return dest;
+}
+
+char *strncpy_fast(char *restrict dest, const char *restrict src, size_t n) {
+    size_t i = 0;
+    for (; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+    if (i < n)
+        dest[i] = '\0';
+
+    return dest;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        if (s1[i] != s2[i])
+            return s1[i] - s2[i];
+        if (s1[i] == '\0' || s2[i] == '\0')
             break;
     }
 
     return 0;
+}
+
+size_t strlen(const char *s) {
+    size_t i = 0;
+    while (s[i] != '\0')
+        i++;
+
+    return i;
 }
