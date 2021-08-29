9377286fa5b3e6e74d455643a782c0560fb79b33
riteme <riteme@qq.com>
2021-08-29 22:44:25 +0800

    aarch64: add more intrinsics.

--- src/aarch64/intrinsic.h
+++ src/aarch64/intrinsic.h
@@ -40,6 +40,11 @@ static inline void arch_dsb_sy() {
     asm volatile("dsb sy" ::: "memory");
 }
 
+static inline void arch_fence() {
+    arch_dsb_sy();
+    arch_isb();
+}
+
 // for `device_get/put_*`, there's no need to protect them with architectual
 // barriers, since they are intended to access device memory regions. These
 // regions are already marked as nGnRnE in `kernel_pt`.
@@ -55,6 +60,59 @@ static inline uint32_t device_get_uint32(uint64_t addr) {
     uint32_t value = *(volatile uint32_t *)addr;
     compiler_fence();
     return value;
+}
+
+// read Exception Syndrome Register (EL1).
+static inline uint64_t arch_get_esr() {
+    uint64_t result;
+    arch_fence();
+    asm volatile("mrs %[x], esr_el1" : [x] "=r"(result));
+    arch_fence();
+    return result;
+}
+
+// reset Exception Syndrome Register (EL1) to zero.
+static inline void arch_reset_esr() {
+    arch_fence();
+    asm volatile("msr esr_el1, %[x]" : : [x] "r"(0ll));
+    arch_fence();
+}
+
+// read Exception Link Register (EL1).
+static inline uint64_t arch_get_elr() {
+    uint64_t result;
+    arch_fence();
+    asm volatile("mrs %[x], elr_el1" : [x] "=r"(result));
+    arch_fence();
+    return result;
+}
+
+// set vector base (virtual) address register (EL1).
+static inline void arch_set_vbar(void *ptr) {
+    arch_fence();
+    asm volatile("msr vbar_el1, %[x]" : : [x] "r"(ptr));
+    arch_fence();
+}
+
+// flush TLB entries.
+static inline void arch_tlbi_vmalle1is() {
+    arch_fence();
+    asm volatile("tlbi vmalle1is");
+    arch_fence();
+}
+
+// set Translation Table Base Register 0 (EL1).
+static inline void arch_set_ttbr0(uint64_t addr) {
+    arch_fence();
+    asm volatile("msr ttbr0_el1, %[x]" : : [x] "r"(addr));
+    arch_tlbi_vmalle1is();
+}
+
+// set Translation Table Base Register 1 (EL1).
+static inline void arch_set_ttbr1(uint64_t addr) {
+    arch_fence();
+    asm volatile("msr ttbr1_el1, %[x]" : : [x] "r"(addr));
+    arch_tlbi_vmalle1is();
 }
 
 void delay_us(uint64_t n);
