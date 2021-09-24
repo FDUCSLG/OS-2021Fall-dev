b926873e2110389bdbd6ee39a5090dcbf09379f6
hatsunehan <hatsunehan@foxmail.com>
2021-09-24 10:39:46 +0800

    add mm.h

---
+++ src/core/memory_manage.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef _CORE_MEMORY_MANAGE_
+#define _CORE_MEMORY_MANAGE_
+
+typedef struct {
+    void *memmory_manager;
+    void (*page_init)(void *, void*, void*);
+    void *(*page_alloc)(void *);
+    void (*page_free)(void *, void *);
+} MemmoryManagerTable;
+
+typedef struct  {
+    void *next;
+    void *start, *end;
+} FreeList;
+
+void init_memory_manager();
+void free_range(void *, void *);
+void *kalloc();
+void kfree(void *);
+
+#endif
