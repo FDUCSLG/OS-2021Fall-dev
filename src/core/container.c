535257fb0cf761dc5f598a9e870aaca281eef0e1
Tan-YiFan <15711657500@163.com>
2021-11-04 16:11:37 +0800

    temp

--- src/core/container.c
+++ src/core/container.c
@@ -1,5 +1,11 @@
 #include <core/container.h>
 
-struct container *root_container;
+struct container root_container;
 
-void spawn_init_container() {}
+void init_container() {
+	
+}
+
+int alloc_resource() {
+	
+}
