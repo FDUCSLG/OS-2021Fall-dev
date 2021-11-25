fb138024bed0be6fae72635ce2c9a26823066d39
Tan-YiFan <15711657500@163.com>
2021-11-25 14:43:32 +0800

    sd: reuse `list.h` in `sd.h`

--- src/common/buf.h
+++ src/common/buf.h
@@ -2,6 +2,7 @@
 
 #include <common/defines.h>
 #include <common/string.h>
+#include <common/list.h>
 
 #define BSIZE   512
 
@@ -14,7 +15,8 @@ struct buf {
     u8 data[BSIZE]; // 1B*512
 
     /* TODO: Your code here. */
-    struct buf* qnext;
+    // struct buf* qnext;
+    ListNode *node;
 };
 
 static inline void
@@ -23,35 +25,40 @@ init_buflist(struct buf* head)
     head->blockno = 0;
     head->flags = 0;
     memset(head->data, 0, sizeof(head->data));
-    head->qnext = NULL;
+    // head->qnext = NULL;
+    init_list_node(head->node);
 }
 
 static inline void
 buflist_push(struct buf* n,
     struct buf* head)
 {
-    struct buf* tmp = head;
+    /* struct buf* tmp = head;
     while (tmp->qnext != NULL)
         tmp = tmp->qnext;
 
     tmp->qnext = n;
-    n->qnext = NULL;
+    n->qnext = NULL; */
+    merge_list(head->node, n->node);
 }
 
 static inline struct buf*
 buflist_front(struct buf* head)//head != NULL
 {
-    return head->qnext;
+    // return head->qnext;
+	return container_of(head->node->next, struct buf, node);
 }
 
 static inline void
 buflist_pop(struct buf* head)//head != NULL
 {
-    head->qnext = (head->qnext)->qnext;
+    // head->qnext = (head->qnext)->qnext;
+    detach_from_list(head->node);
 }
 
 static inline int
 buflist_empty(struct buf* head)
 {
-    return (head->qnext == NULL);
+    // return (head->qnext == NULL);
+    return head->node->next == head->node;
 }
